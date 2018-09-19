69e7de070ba84fb782d5077426dab9639387a921
NakWon CHOI <ch0494@gmail.com>
2018-09-19 21:35:51 +0900

    Get From JSon 구현

--- UABE_LoadViewModifyAssets/UABE_LoadViewModifyAssets/Project1.h
+++ UABE_LoadViewModifyAssets/UABE_LoadViewModifyAssets/Project1.h
@@ -1,3 +1,9 @@
 #pragma once
+#include "json/json.h"
 using namespace std;
+
 bool LoadAssetsFile(wstring gameFolderPath, wstring assetsFileName);
+
+AssetTypeValueField * GetAssetTypeValueFieldFromJsonRecursive(AssetTypeTemplateField * assetTypeTemplateField, Json::Value json);
+
+AssetTypeValueField * GetAssetTypeValueFieldArrayFromJson(AssetTypeTemplateField * assetTypeTemplateField, Json::Value json);

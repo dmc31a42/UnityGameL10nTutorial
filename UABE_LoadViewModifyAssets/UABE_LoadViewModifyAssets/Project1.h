90a233521103bcd961df2d273a638776aedc6bf4
NakWon CHOI <ch0494@gmail.com>
2018-09-22 19:18:26 +0900

    폰트 자동 패치 완료, 클래스파일 선택하는거 구현해야함. 필요없는 함수 정리해야함.

--- UABE_LoadViewModifyAssets/UABE_LoadViewModifyAssets/Project1.h
+++ UABE_LoadViewModifyAssets/UABE_LoadViewModifyAssets/Project1.h
@@ -7,3 +7,7 @@ bool LoadAssetsFile(wstring gameFolderPath, wstring assetsFileName);
 AssetTypeValueField * GetAssetTypeValueFieldFromJsonRecursive(AssetTypeTemplateField * assetTypeTemplateField, Json::Value json);
 
 AssetTypeValueField * GetAssetTypeValueFieldArrayFromJson(AssetTypeTemplateField * assetTypeTemplateField, Json::Value json);
+
+void ReplaceMaterial(string assetsName, AssetsFileTable * assetsFileTable, AssetFileInfoEx * assetFileInfoEx, AssetTypeInstance * assetTypeInstance, float _TextureHeight, float _TextureWidth);
+
+void ReplaceAtlas(string assetsname, AssetsFileTable * assetsFileTable, AssetFileInfoEx * assetFileInfoEx, AssetTypeInstance * assetTypeInstance, int m_CompleteImageSize, string atlasPath, int m_Width, int m_Height);

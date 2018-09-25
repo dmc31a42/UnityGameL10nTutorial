db960c680cf2af6c56bc95e27fadc59fad12e334
NakWon CHOI <ch0494@gmail.com>
2018-09-25 15:05:36 +0900

    리네이밍

--- UABE_LoadViewModifyAssets/UABE_LoadViewModifyAssets/Project1.h
+++ UABE_LoadViewModifyAssets/UABE_LoadViewModifyAssets/Project1.h
@@ -2,7 +2,14 @@
 #include "json/json.h"
 using namespace std;
 
-bool LoadAssetsFile(wstring gameFolderPath, wstring assetsFileName);
+struct AssetLogicalPath {
+	string AssetsName;
+	string AssetName;
+	string ContainerPath;
+	bool hasContainerPath;
+};
+
+bool LoadAssetsFile(wstring gameFolderPath, string assetsFileName);
 
 AssetTypeValueField * GetAssetTypeValueFieldFromJsonRecursive(AssetTypeTemplateField * assetTypeTemplateField, Json::Value json);
 
@@ -11,3 +18,17 @@ AssetTypeValueField * GetAssetTypeValueFieldArrayFromJson(AssetTypeTemplateField
 void ReplaceMaterial(string assetsName, AssetsFileTable * assetsFileTable, AssetFileInfoEx * assetFileInfoEx, AssetTypeInstance * assetTypeInstance, float _TextureHeight, float _TextureWidth);
 
 void ReplaceAtlas(string assetsname, AssetsFileTable * assetsFileTable, AssetFileInfoEx * assetFileInfoEx, AssetTypeInstance * assetTypeInstance, int m_CompleteImageSize, string atlasPath, int m_Width, int m_Height);
+
+string readFile2(const string & fileName);
+
+bool copyFile(const char * SRC, const char * DEST);
+
+bool copyFile(const wchar_t * SRC, const wchar_t * DEST);
+
+vector<wstring> get_all_files_names_within_folder(wstring filter);
+
+bool CreateProcessCustom(wstring commandLine);
+
+bool ProcessResourceAndMonoManger(AssetsFileTable * globalgamemanagersTable, wstring gameFolderPath, string globalgamemanagersName);
+
+bool LoadFindMonoClassNameFromMonoScriptPathId(AssetsFileTable * globalgamemanagersAssetsTable);
